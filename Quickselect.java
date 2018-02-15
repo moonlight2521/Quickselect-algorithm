ed2f344c84d00ddf1575b9843a9d6218170a8cb6
Zun Lin <zunliang2008@gmail.com>
2018-02-15 18:24:33 -0500

    Update and rename OptimalPower.java to Quickselect.java

--- Quickselect.java
+++ Quickselect.java
@@ -3,7 +3,7 @@
  */
 import java.util.Map;
 import java.util.Scanner;
-public class OptimalPower {
+public class Quickselect {
 
     public static void main(String[] args){
     Scanner in = new Scanner(System.in);
